b21c061c0d4ddc51612f1e42fc2f6800ac2b529b
Ralph Cacho <cacho.rc@gmail.com>
2014-10-31 18:08:50 -0700

    Add functionality to send messages

--- app/src/main/java/com/ralphiz/ribbit/MainActivity.java
+++ app/src/main/java/com/ralphiz/ribbit/MainActivity.java
@@ -266,6 +266,19 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
                 mediaScanIntent.setData(mMediaUri);
                 sendBroadcast(mediaScanIntent);
             }
+
+            Intent recipientsIntent = new Intent(this, RecipientsActivity.class);
+            recipientsIntent.setData(mMediaUri);
+
+            String fileType;
+            if (requestCode == TAKE_PHOTO_REQUEST || requestCode == PICK_PHOTO_REQUEST) {
+                fileType = ParseConstants.TYPE_IMAGE;
+            }
+            else {
+                fileType = ParseConstants.TYPE_VIDEO;
+            }
+            recipientsIntent.putExtra(ParseConstants.KEY_FILE_TYPE, fileType);
+            startActivity(recipientsIntent);
         }
         else if (resultCode != RESULT_CANCELED) {
             Toast.makeText(this, R.string.error_general, Toast.LENGTH_LONG).show();
