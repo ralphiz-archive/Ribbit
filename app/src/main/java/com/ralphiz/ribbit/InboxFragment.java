04f5eac09860feb54c0ab3fb7dd7e5fd56310dd6
Ralph Cacho <cacho.rc@gmail.com>
2014-11-03 20:16:37 -0800

    -Add functionality for messages to 'self-destruct' (removed from the recipient's inbox) -File is not deleted on Parse backend -Scroll bug fix

--- app/src/main/java/com/ralphiz/ribbit/InboxFragment.java
+++ app/src/main/java/com/ralphiz/ribbit/InboxFragment.java
@@ -17,6 +17,7 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -57,10 +58,16 @@ public class InboxFragment extends ListFragment {
                         usernames[i] = message.getString(ParseConstants.KEY_SENDER_NAME);
                         i++;
                     }
-                    MessageAdapter adapter = new MessageAdapter(
-                            getListView().getContext(),
-                            mMessages);
-                    setListAdapter(adapter);
+                    if (getListView().getAdapter() == null) {
+                        MessageAdapter adapter = new MessageAdapter(
+                                getListView().getContext(),
+                                mMessages);
+                        setListAdapter(adapter);
+                    }
+                    else {
+                        // Refill the adapter!
+                        ((MessageAdapter) getListView().getAdapter()).refill(mMessages);
+                    }
                 }
             }
         });
@@ -75,7 +82,7 @@ public class InboxFragment extends ListFragment {
         ParseFile file = message.getParseFile(ParseConstants.KEY_FILE);
         Uri fileUri = Uri.parse(file.getUrl());
 
-        if(messageType.equals(ParseConstants.TYPE_IMAGE)) {
+        if (messageType.equals(ParseConstants.TYPE_IMAGE)) {
             // View the image
             Intent intent = new Intent(getActivity(), ViewImageActivity.class);
             intent.setData(fileUri);
@@ -86,6 +93,25 @@ public class InboxFragment extends ListFragment {
             Intent intent = new Intent(Intent.ACTION_VIEW, fileUri);
             intent.setDataAndType(fileUri, "video/*");
             startActivity(intent);
+        }
+
+        // Delete it!
+        List<String> ids = message.getList(ParseConstants.KEY_RECIPIENT_IDS);
+
+        if (ids.size() == 1) {
+            // Last recipient - delete the whole thing!
+            message.deleteInBackground();
+        }
+        else {
+            // Remove the recipient and save.
+            ids.remove(ParseUser.getCurrentUser().getObjectId());
+
+            // Clunky way to remove a recipient
+            ArrayList<String> idsToRemove = new ArrayList<String>();
+            idsToRemove.add(ParseUser.getCurrentUser().getObjectId());
+
+            message.removeAll(ParseConstants.KEY_RECIPIENT_IDS, idsToRemove);
+            message.saveInBackground();
         }
     }
 }
